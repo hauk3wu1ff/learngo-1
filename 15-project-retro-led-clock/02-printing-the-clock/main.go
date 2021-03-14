acbbfc381155b8e8986d234b5ef4e1ca901f6a56
hauk3wu1ff <hauk3wu1ff@gmail.com>
2021-03-14 12:18:40 +0000

    Retro-Clock step 1 and 2

--- 15-project-retro-led-clock/02-printing-the-clock/main.go
+++ 15-project-retro-led-clock/02-printing-the-clock/main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -95,13 +96,43 @@ func main() {
 		"███",
 	}
 
+	separator := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
 	digits := [...]placeholder{
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	for line := range digits[0] {
-		for digit := range digits {
-			fmt.Print(digits[digit][line], "  ")
+	ct := time.Now()
+	h, m, s := ct.Hour(), ct.Minute(), ct.Second()
+	fmt.Printf("%d:%d:%d\n", h, m, s)
+	hHigh := h / 10
+	hLow := h - hHigh*10
+	mHigh := m / 10
+	mLow := m - mHigh*10
+	sHigh := s / 10
+	sLow := s - sHigh*10
+	fmt.Printf("%d%d:%d%d:%d%d\n", hHigh, hLow,
+		mHigh, mLow, sHigh, sLow)
+
+	clock := [8]placeholder{
+		digits[hHigh],
+		digits[hLow],
+		separator,
+		digits[mHigh],
+		digits[mLow],
+		separator,
+		digits[sHigh],
+		digits[sLow],
+	}
+	for line := range clock[0] {
+		for _, digit := range clock {
+			fmt.Print(digit[line], " ")
 		}
 		fmt.Println()
 	}
