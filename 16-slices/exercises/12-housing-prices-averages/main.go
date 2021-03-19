44b85e33d6a6ed025c3fdd164e2dfd6f1411cdf2
hauk3wu1ff <hauk3wu1ff@gmail.com>
2021-03-19 10:15:50 +0000

    16 slices exercise 12

--- 16-slices/exercises/12-housing-prices-averages/main.go
+++ 16-slices/exercises/12-housing-prices-averages/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Housing Prices and Averages
 //
@@ -41,6 +47,76 @@ Istanbul,500,10,5,1000000`
 		separator = ","
 	)
 
-	// Solve this exercise by using your previous solution for
-	// the "Housing Prices" exercise.
+	var (
+		locations []string
+		size      []int
+		beds      []int
+		baths     []int
+		prices    []int
+	)
+
+	hs := strings.Split(header, separator)
+	ds := strings.Split(data, "\n")
+
+	for r, rv := range ds {
+		for c, fv := range strings.Split(rv, ",") {
+			switch c {
+			case 0:
+				locations = append(locations, fv)
+			case 1:
+				i, err := strconv.Atoi(fv)
+				if err != nil {
+					fmt.Printf("Row/Column %d/%d should be int\n", r, c)
+					return
+				}
+				size = append(size, i)
+			case 2:
+				i, err := strconv.Atoi(fv)
+				if err != nil {
+					fmt.Printf("Row/Column %d/%d should be int\n", r, c)
+					return
+				}
+				beds = append(beds, i)
+			case 3:
+				i, err := strconv.Atoi(fv)
+				if err != nil {
+					fmt.Printf("Row/Column %d/%d should be int\n", r, c)
+					return
+				}
+				baths = append(baths, i)
+			case 4:
+				i, err := strconv.Atoi(fv)
+				if err != nil {
+					fmt.Printf("Row/Column %d/%d should be int\n", r, c)
+					return
+				}
+				prices = append(prices, i)
+			}
+		}
+	}
+	fmt.Printf("%-15s%-15s%-15s%-15s%-15s\n", hs[0], hs[1], hs[2], hs[3], hs[4])
+	fmt.Printf("%s\n", strings.Repeat("=", 75))
+	for i := 0; i < len(ds); i++ {
+		fmt.Printf("%-15s", locations[i])
+		fmt.Printf("%-15d", size[i])
+		fmt.Printf("%-15d", beds[i])
+		fmt.Printf("%-15d", baths[i])
+		fmt.Printf("%-15d\n", prices[i])
+	}
+	fmt.Printf("%s\n", strings.Repeat("=", 75))
+	fmt.Printf("%-15s%-15.2f%-15.2f%-15.2f%-15.2f\n", "", average(size), average(beds), average(baths), average(prices))
+}
+
+// Calculates the average of an int slice.
+// Returns 0 for a nil or empty slice
+func average(s []int) float64 {
+	var sum int
+	if len(s) > 0 {
+		for _, v := range s {
+			sum += v
+		}
+		return float64(sum) / float64(len(s))
+	} else {
+		return 0
+	}
 }
